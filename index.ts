19041364d3df7ca10b8936d33faeda46c4531890
ICS4U-1-2024 <ics4u-1-2024@mths.ca>
2024-09-23 17:46:39 +0000

    update

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+/**
+ * The program shows how to get input
+ *   and deal with numbers.
+ *
+ * By:      Mr. Coxall
+ * Version: 1.0
+ * Since:   2020-01-01
+ */
+
+import { createPrompt } from 'bun-promptx'
+
+const name = createPrompt('What is your name? ')
+console.log(`Hey there ${String(name.value)}`)
+
+const ageString = createPrompt('What is your age? ')
+let ageNumber = parseInt(ageString.value)
+ageNumber = ageNumber - 10
+console.log(`Age is > ${ageNumber}`)
+
+console.log('\nDone.')
